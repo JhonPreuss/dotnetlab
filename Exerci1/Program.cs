af3865f5d141d4b2fedc974f1f9a3c274ec7bf39
JP <jonathan.preuss@redes.ufsm.br>
2021-02-16 17:46:28 -0300

    Testando os inputs de aplicações consoles

---
+++ Exerci1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exerci1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Manda um número ae");
+            string x = Console.ReadLine();
+            num = Convert.ToInt32(x);
+            Console.WriteLine("Convertidão meu chapa -> "+num);
+        }
+    }
+}
