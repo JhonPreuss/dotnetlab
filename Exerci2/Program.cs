d499cfc7d1223d09c016967722c2a35ca6bacde1
JP <jonathan.preuss@redes.ufsm.br>
2021-02-17 12:37:25 -0300

    exercicios reajuste salarial

---
+++ Exerci2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+/*Faça um algoritmo que calcule e exiba o salário reajustado de dez funcionários de acordo com a seguinte regra: Salário até 300, reajuste de 50%; Salários maiores que 300, reajuste de 30%.*/
+namespace Exerci2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Salario = 0.0;
+            Double SalarioAjustado = 0;
+            Console.WriteLine("Digite o salário a ser reajustado: \n");
+            Salario = Convert.ToDouble(Console.ReadLine());
+            if(Salario <= 300.00)
+            {
+                SalarioAjustado = Salario + (Salario*0.5);
+            }
+            else
+            {
+                SalarioAjustado = Salario + (Salario*0.3);
+            }
+            Console.WriteLine("Salário Antigo: R$ "+Salario+"\n Salário reajustado: R$ "+SalarioAjustado);
+
+
+        }
+    }
+}
